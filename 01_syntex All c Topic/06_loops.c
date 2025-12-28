f5e59351ee981f09c0e1943b30e7d43ad624a2f6
SHAKIL AHMED <shakil.ahmed.mobin@gmail.com>
2025-12-28 12:34:31 +0600

    Update 06_loops.c

--- 01_syntex All c Topic/06_loops.c
+++ 01_syntex All c Topic/06_loops.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
-
-int main() {
-    // For Loop (Fixed iterations)
-    for (int i = 1; i <= 5; i++) {
-        printf("Count: %d\n", i);
+int main () {
+    // ⭐ For loop
+    for (int i = 1; i <= 100; i++) {
+        printf("%d\n", i);
     }
-
-    // While Loop (Condition based)
-    int j = 1;
-    while (j <= 5) {
-        printf("While: %d\n", j);
-        j++;
+    // ⭐ While loop
+    int i = 1;
+    while (i <= 100) {
+        printf("%d\n", i);
+        i++;
     }
+    // ⭐ Do-While loop
+    i = 1;
+    do {
+        printf("%d\n", i);
+        i++;
+    } while (i <= 100);
+
     return 0;
 }
